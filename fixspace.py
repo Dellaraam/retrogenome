04ddaefec5c82ecd062ef8761f9e8d888226c5f4
Dellaraam <136400422+Dellaraam@users.noreply.github.com>
2024-03-30 20:00:04 -0700

    fix

--- fixspace.py
+++ fixspace.py
@@ -6,7 +6,7 @@ with gzip.open(sys.argv[1], 'rt') as fp:
 	for line in fp:
 		if line.startswith(">"):
 			line.replace(" ", "_")
-		if line.startswith("AT"):
+		if line.startswith("#"):
 			line.replace(" ", "_")
 		print(line, end="")
 
