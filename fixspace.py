6d2e09593700b976d17bc36f8df04c9b4eb362da
Dellaraam <136400422+Dellaraam@users.noreply.github.com>
2024-03-30 20:12:48 -0700

    fix

--- fixspace.py
+++ fixspace.py
@@ -6,7 +6,5 @@ with gzip.open(sys.argv[1], 'rt') as fp:
 	for line in fp:
 		if line.startswith(">"):
 			line.replace(" ", "_")
-		if line.startswith("#"):
-			line.replace(" ", "_")
 		print(line, end="")
 
