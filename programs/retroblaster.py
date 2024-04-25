64597f323c1a66854af515e8ad03ed3a2bbdfeb3
Dellaraam <136400422+Dellaraam@users.noreply.github.com>
2024-04-24 17:05:54 -0700

    retroblaster.py

---
+++ programs/retroblaster.py
@@ -0,0 +1,25 @@
+from subprocess import run
+import argparse
+
+
+# setup
+parser = argparse.ArgumentParser(description='Brief description of program.')
+
+# positional arguments (always required)
+parser.add_argument('file', type=str, metavar='<path>', help='some file')
+
+# optional arguments with default parameters
+parser.add_argument('query', type=str, metavar='<path>')
+parser.add_argument('database', type=str, metavar='<path>')
+parser.add_argument('--hsp', required=False, type=str, default='2000')
+parser.add_argument('--n', required=False, type=str, defualt='20')
+parser.add_argument('--cpu', required=False, type=str, default='15')
+parser.add_argument('--evalue', reqired=False, type=str, default='1e-30')
+parser.add_argument('--mformat', required=False, type=str, default='3')
+
+
+
+output = sys.argv[1]
+alignnments = run(f'ab-blast-20200317-linux-x64/tblastn* {arg.query} {arg.database}  -topcomboN={arg.n} -hspsepsmax={arg.hsp} -cpus={arg.cpu} -E {arg.evalue} -mformat {arg.mformat} & > {output}')
+
+	
