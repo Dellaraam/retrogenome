29e77c03ad3ee094a1be54aba8209ae7ff7db4ea
Dellaraam <136400422+Dellaraam@users.noreply.github.com>
2024-06-22 21:58:04 -0700

    info

---
+++ programs/retro_info.py
@@ -0,0 +1,41 @@
+import sys 
+import  gzip
+
+retrolist = {}
+parentlist = {}
+
+with open(sys.argv[1]) as fp:
+	for line1 in fp:
+		l = line1.split()
+		retrogene = l[0]
+		parent = l[1]
+		#print(retrogene)
+		retrolist[retrogene] = True
+		parentlist[parent] = True	
+
+f1 = open("retro_introns.fa", "w")
+f2 = open("parent_introns.fa", "w")
+
+with gzip.open(sys.argv[2], "rt") as fp2:
+	for line2 in fp2:
+		l2 = line2.split()
+		g = l2[0].split(".")
+		gene = g[0]
+		read = g[1]
+		start = l2[1]
+		end = l2[2]
+		intron = l2[5]
+		#print(gene)
+		for retro in retrolist.keys():
+			if gene == retro:
+				f1.write(f'>{gene}.{read} {start}-{end}\n')
+				f1.write(f'{intron}\n')
+			if parent in parentlist.keys():
+				f1.write(f'>{gene}.{read} {start}-{end}\n')
+				f1.write(f'{intron}\n')
+	
+		
+#print(retrolist, parentlist)
+	
+		
+#-W 7 -Q 3 -R 1 -M 1 -N -1
